37fa8b3de844bced9576d1e38a914f03128c4242
fernando <fernando100@windowslive.com>
2025-04-10 22:33:16 -0300

    inclusão do swagger para documentação - 10/04/2025

--- tc-backend/utilities/api.py
+++ tc-backend/utilities/api.py
@@ -1,16 +1,36 @@
 from flask import Flask, jsonify
 import sys
 import os
+from flasgger import Swagger
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from embrapa_scraper.scraping import Scraping
 
 dados = Scraping()
-
 app = Flask(__name__)
+swagger = Swagger(app)  ## Testar: http://localhost:5000/apidocs/
 
-@app.route("/api")
+@app.route("/api", methods=["GET"])
 def Home():
-    return {"dados":dados}
+    """
+    GET - EXIBINDO DADOS DA API
+    ---
+    parameters:
+      - name: N/A
+        in: query
+        type: JSON
+        required: true
+        description: dados embrapa
+    responses:
+      200:
+        description: dados embrapa
+        schema:
+          type: object
+          properties:
+            resultado:
+              type: JSON
+              example: dados
+    """
+    return  {"dados":dados}
     
 if __name__ == "__main__":
     app.run(debug=True)
