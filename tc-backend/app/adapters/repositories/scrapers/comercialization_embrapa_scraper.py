adc4f57f2e1d057bf9ef54c6fa096f6fc5ec9934
Alexander Martins <alexander@inovart.com.br>
2025-05-02 19:26:19 -0300

    Ajuste de bug

--- tc-backend/app/adapters/repositories/scrapers/comercialization_embrapa_scraper.py
+++ tc-backend/app/adapters/repositories/scrapers/comercialization_embrapa_scraper.py
@@ -4,7 +4,7 @@ from app.adapters.repositories.scrapers.embrapa_base_scraper import EmbrapaBaseS
 from app.domain.entities.product import Product
 from app.domain.repositories.base_repository import BaseRepository
 
-class ComercializationEmbrapaScraper(BaseRepository):
+class ComercializationEmbrapaScraper(EmbrapaBaseScraper, BaseRepository):
     def fetch_all(self, year: int) -> List[Product]:
         url = f"{BASE_URL}?ano={year}&opcao=opt_04"
         response = self._fetch_page(url)
