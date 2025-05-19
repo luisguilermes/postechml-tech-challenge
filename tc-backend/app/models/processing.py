0ad043bc934b15df9291c278d8388e1c7fe69a25
Alexander Martins <alexander@inovart.com.br>
2025-05-19 18:22:34 -0300

    Inclusão de processamento

---
+++ tc-backend/app/models/processing.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Processing(BaseModel):
+    category: str
+    year: int
+    grow: str
+    sub_grow: str
+    amount: float
+    unit: str
+    source: str
+    collected_at: datetime
+
+    def to_dict(self):
+        return {
+            "category": self.category,
+            "year": self.year,
+            "grow": self.grow,
+            "sub_grow": self.sub_grow,
+            "amount": self.amount,
+            "unit": self.unit,
+            "source": self.source,
+            "collected_at": self.collected_at.isoformat(),
+        }
